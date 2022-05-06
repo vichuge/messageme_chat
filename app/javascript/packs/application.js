e4610b259d3dd111355a30e63bd0792121a788f8
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-05-06 08:54:22 -0500

    Add navbar and all the funcionality to him

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,20 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("../stylesheets/application.scss")
+import "jquery"
+import '@doabit/semantic-ui-sass'
+// import '@doabit/semantic-ui-sass/src/scss/semantic-ui.scss'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).ready(function () {
+  console.log('hello world');
+  $('h1').css('color', 'red');
+});
+
+$(document).on('turbolinks:load', function () {
+  $('.ui.dropdown').dropdown();
+})
