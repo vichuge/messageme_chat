acb621ad058bd71c51357e5d2686abe8b6fc7183
vichuge <victor.hugo.pacheco.flores@gmail.com>
2022-05-09 12:32:49 -0500

    Add all messages funcionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,4 +23,7 @@ $(document).ready(function () {
 
 $(document).on('turbolinks:load', function () {
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function () {
+    $(this).closest('.message').transition('fade');
+  });
 })
